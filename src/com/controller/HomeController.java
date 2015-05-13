07535e2d0c46dc1aa2e5cb34cf844ecc9a04367a
Diaconu Stefan <diaconustefandaniel@gmail.com>
2015-05-13 17:51:44 +0300

    spring+hibernate configuration.

---
+++ src/com/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping(value="/")
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public ModelAndView metoda() {
+		
+		ModelAndView model=new ModelAndView("blabla");
+	    System.out.println("Sunt aici");
+	    return model;
+		//return new ModelAndView("home", "courts", courtService.findAll());
+	}
+	
+	@RequestMapping(value="home", method = RequestMethod.GET)
+	public String metoda2() {
+		
+		
+	    System.out.println("Sunt aici");
+	    return "blabla";
+		//return new ModelAndView("home", "courts", courtService.findAll());
+	}
+	
+}
